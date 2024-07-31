869586d7e07394ee31c9ecc479d83f4765a4c4fa
errafiq-1337 <abdelilah.errafiq.1337@gmail.com>
2024-07-31 15:01:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,11 @@
  */
 int main(void)
 {
-    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char msg1[] = "and that piece of art is useful\"";
+    char msg2[] = " - Dora Korpar, 2015-10-19\n";
 
-    write(2, msg, sizeof(msg) - 1);
+    write(2, msg1, sizeof(msg1) - 1);
+    write(2, msg2, sizeof(msg2) - 1);
     return (1);
 }
+
