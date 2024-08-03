3c87525098a3d7aa056bc234bc528fe50c2f59f4
errafiq-1337 <abdelilah.errafiq.1337@gmail.com>
2024-08-03 11:50:40 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - for a program that prints the alphabet in lowercase
+ * return: 0
+**/
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
