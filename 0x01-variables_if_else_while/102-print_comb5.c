632d2b6cc7ff706de5c9b87da3e6d7919f576a76
errafiq-1337 <abdelilah.errafiq.1337@gmail.com>
2024-08-04 10:02:46 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
 
 int main(void)
 {
-        int i = 0;
-        int j;
+	int i = 0;
+	int j;
 
-        while (i >= 0 && i <= 99)
-        {
-                j = i + 1;
-                while (j >= 0 && j <= 99)
-                {
-                        putchar(i / 10 + 48);
-                        putchar(i % 10 + 48);
-                        putchar(' ');
-                        putchar(j / 10 + 48);
-                        putchar(j % 10 + 48);
-                        if (i != 98 || j != 99)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        j++;
-                }
-                i++;
-        }
-        return (0);
+	while (i >= 0 && i <= 99)
+	{
+		j = i + 1;
+		while (j >= 0 && j <= 99)
+		{
+			putchar(i / 10 + 48);
+			putchar(i % 10 + 48);
+			putchar(' ');
+			putchar(j / 10 + 48);
+			putchar(j % 10 + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
