f587ccfd9f8685ff965f8bddfba2c4a7800b7171
errafiq-1337 <abdelilah.errafiq.1337@gmail.com>
2024-08-07 17:00:51 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - for print alpha
  */
 void print_alphabet(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	while(alpha[i])
+	while (alpha[i])
 		_putchar(alpha[i++]);
 	_putchar('\n');
 }
