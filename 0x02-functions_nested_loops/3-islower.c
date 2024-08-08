b2b541ba5e1cc5b123199121d6e804a2d9c7557b
errafiq-1337 <abdelilah.errafiq.1337@gmail.com>
2024-08-07 17:31:18 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * _islower - for checks for lowercase
+ * c - parameter or member
  * Return: 0 if otherwise and 1 if c is lowercase
 **/
 
 int _islower(int c)
 {
 	if (c >= 65 && c <= 92)
-		return(0);
+		return (0);
 	else
-		return(1);
+		return (1);
 }
